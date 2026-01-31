f8d308289f3cad332f78d9003e4d036277933dfb
Rohit Singh <rohitsingh@example.com>
2026-01-31 21:50:39 +0530

    practice first to build app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,21 @@
- <h1>hello everone</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+    <div class="min-h-screen bg-gray-900 flex items-center justify-center">
+        <div class="bg-white p-6 rounded-lg">
+            <h1 class="text-red-500 text-4xl font-bold">
+                Hello Boy
+            </h1>
+            <p class='h-40 text-amber-300 w-96 bg-amber-500'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas cum deserunt nesciunt laudantium, non numquam ex deleniti amet sint perferendis tenetur atque eum cupiditate voluptate beatae exercitationem voluptatem id fugit quam possimus, incidunt rerum necessitatibus quos. Sapiente, blanditiis ducimus? Nihil dicta quaerat quisquam laborum ea?
+            </p>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home route → returns the welcome view
 Route::get('/', function () {
     return view('welcome');
+});
+
+// User route with required parameter
+Route::get('/users', function () {
+ return view('users');
 });
